6baf88dd41d811dcdd0b5ecec802be8c0d8c968b
howkeye17 <howkeye17@gmail.com>
2021-09-25 13:19:12 +0300

    Initial Commit

---
+++ Weather app (Innowise iOS test task)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather app (Innowise iOS test task)
+//
+//  Created by Valera Vasilevich on 25.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
