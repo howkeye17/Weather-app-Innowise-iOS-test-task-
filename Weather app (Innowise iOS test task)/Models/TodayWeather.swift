d709814089e16219d71fdb8571f0fbb4874676b3
howkeye17 <howkeye17@gmail.com>
2021-09-27 21:31:19 +0300

    Implementation of TodayWeather struct for initialisation of all properties for TodayVC

---
+++ Weather app (Innowise iOS test task)/Models/TodayWeather.swift
@@ -0,0 +1,72 @@
+//
+//  TodayWeather.swift
+//  Weather app (Innowise iOS test task)
+//
+//  Created by Valera Vasilevich on 27.09.21.
+//
+
+import Foundation
+
+struct TodayWeather {
+
+    let cityName: String
+    
+    let temperature: Double
+    var temperatureString: String {
+        return String(format: "%.0f", temperature)
+    }
+    
+    let weatherStatus: String
+    
+    let conditionCode: Int
+    var systemIconNameString: String {
+        switch conditionCode {
+        case 200...232: return "cloud.bolt.rain.fill"
+        case 300...321: return "cloud.drizzle.fill"
+        case 500...531: return "cloud.rain.fill"
+        case 600...622: return "cloud.snow.fill"
+        case 701...781: return "smoke.fill"
+        case 800: return "sun.max"
+        case 801...804: return "cloud.fill"
+        default: return "nosign"
+        }
+    }
+    
+    let humidity: Int
+    let rainAmount: Double
+    let pressure: Int
+    
+    let windSpeed: Double
+    var windKmH: Int {
+        return (Int(windSpeed) * 3600) / 1000
+    }
+    
+    let windDirection: Int
+    var compassDirection: String {
+        switch windDirection {
+        case 0...22, 339...359: return "N"
+        case 23...67: return "NE"
+        case 68...112: return "E"
+        case 113...157: return "SE"
+        case 158...202: return "S"
+        case 203...248: return "SW"
+        case 249...294: return "W"
+        case 295...338: return "NW"
+        default: return "n/a"
+        }
+    }
+    
+    init?(todayWeatherData: TodayWeatherData) {
+        cityName = todayWeatherData.name
+        temperature = todayWeatherData.main.temp
+        weatherStatus = todayWeatherData.weather.first!.status
+        conditionCode = todayWeatherData.weather.first!.id
+        humidity = todayWeatherData.main.humidity
+        rainAmount = todayWeatherData.rain.rain ?? 0.0
+        pressure = todayWeatherData.main.pressure
+        windSpeed = todayWeatherData.wind.speed
+        windDirection = todayWeatherData.wind.direction
+    }
+}
+
+
--- Weather app (Innowise iOS test task)/Models/TodayWeatherData.swift
+++ Weather app (Innowise iOS test task)/Models/TodayWeatherData.swift
@@ -8,28 +8,24 @@
 import Foundation
 
 struct TodayWeatherData: Codable {
-    
     let name: String  // cityName
     let main: Main
     let wind: Wind
     let rain: Rain
     let weather: [Weather]
 }
-
 struct Main: Codable {
-    let temp: Double
+    let temp: Double //temperature
     let pressure: Int
     let humidity: Int
 }
-
 struct Rain: Codable {
     let rain: Double?  // rain amoun for the last 1h
-
+    
     enum CodingKeys: String, CodingKey {
         case rain = "1h"
     }
 }
-
 struct Weather: Codable {
     let id: Int   // conditionCode for weather status image
     let status: String // status(cloud, sun etc..)
@@ -39,7 +35,6 @@ struct Weather: Codable {
         case status = "main"
     }
 }
-
 struct Wind: Codable {
     let speed: Double // wind speed
     let direction: Int  // wind direction degrees (meteorological)
@@ -48,25 +43,5 @@ struct Wind: Codable {
         case speed
         case direction = "deg"
     }
-
-
-/*
-//MARK: deg to compass
-
-let degDirection: Int
-var compassDirection: String {
-    switch degDirection {
-    case 0...22, 339...359: return "N"
-    case 23...67: return "NE"
-    case 68...112: return "E"
-    case 113...157: return "SE"
-    case 158...202: return "S"
-    case 203...248: return "SW"
-    case 249...294: return "W"
-    case 295...338: return "NW"
-    default: return "n/a"
-    }
-}
-*/
 }
 
